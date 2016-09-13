5b9e9f02a6b91d0b6845b7f50048696bb7013370
Rickard <richen_mjau@hotmail.com>
2016-09-13 11:40:55 +0200

    Update

---
+++ Rickard_test.m
@@ -0,0 +1,13 @@
+%% Implement three functions
+clear all, close all, clf, clc, format compact
+
+% 1. function reach_states = reach(start_states, trans)
+% Returns the forward reachable states of a transition set
+% 2. function coreach_states = coreach(start_states, trans)
+% Returns the coreachable (backward reachable) states of a transition set
+% 3. function aut1aut2 = synch(aut1, aut2)
+% Returns the synchronous composition of two automata
+
+
+
+
